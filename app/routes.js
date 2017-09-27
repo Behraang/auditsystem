3b22c1dacaee80bbf1351eb767511c572eb5d50d
Behrang Yarahmadi <behraang@outlook.com>
2017-09-27 23:57:52 +0200

    added title to tha pages

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.render('index.handlebars'); // load the index.handlebars file
+        res.render('index.handlebars', {title: 'Index'}); // load the index.handlebars file
     });
 
     // =====================================
@@ -14,7 +14,7 @@ module.exports = function(app, passport) {
     app.get('/login', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('login.handlebars', { message: req.flash('loginMessage') }); 
+        res.render('login.handlebars', { message: req.flash('loginMessage'), title: 'Login' }); 
     });
 
     // process the login form
@@ -31,7 +31,7 @@ module.exports = function(app, passport) {
     app.get('/signup', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('signup.handlebars', { message: req.flash('signupMessage') });
+        res.render('signup.handlebars', { message: req.flash('signupMessage'), title: 'Signup' });
     });
 
     // process the signup form
@@ -48,7 +48,8 @@ module.exports = function(app, passport) {
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.handlebars', {
-            user : req.user // get the user out of session and pass to template
+            user : req.user, // get the user out of session and pass to template
+            title: 'Profile'
         });
     });
 
