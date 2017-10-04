82b853f84769ca999d082e2d87d4b5c1ba0c2181
Alessio Bertelle <alessio.bertelle@trivago.com>
2017-10-04 17:00:39 +0200

    completed signup/login start page

--- app/routes.js
+++ app/routes.js
@@ -14,11 +14,11 @@ module.exports = function(app, passport) {
     // =====================================
     app.get('/', function(req, res) {
 
-        res.render('login.handlebars', {
+        res.render('signup.handlebars', {
         message: req.flash('signupMessage'),
         layout: 'layoutA.handlebars',
         user : req.user, // get the user out of session and pass to template
-        title: 'Login'}); // load the index.handlebars file
+        title: 'Signup'}); // load the index.handlebars file
     });
 
     // =====================================
@@ -97,7 +97,7 @@ module.exports = function(app, passport) {
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
         successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/', // redirect back to the signup page if there is an error
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
 
@@ -117,7 +117,7 @@ module.exports = function(app, passport) {
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/', // redirect back to the signup page if there is an error
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
 
@@ -138,7 +138,7 @@ module.exports = function(app, passport) {
     // =====================================
     app.get('/logout', function(req, res) {
         req.logout();
-        res.redirect('/');
+        res.redirect('/login');
     });
 
     // =====================================
